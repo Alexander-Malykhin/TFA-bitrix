2d92964d91906170cbc30abbbd2fa840573330d3
lexander alykhin <your-email@example.com>
2025-05-28 21:37:08 +0300

    Модуль TFA для двухфакторной авторизации

--- tfa/install/php_interface/events/auth/CheckIP.php
+++ tfa/install/php_interface/events/auth/CheckIP.php
@@ -6,46 +6,65 @@ use Bitrix\Main\Application;
 use Bitrix\Main\Loader;
 use Bitrix\Main\UserTable;
 use Bitrix\Security\Mfa\Otp;
+use Bitrix\Security\Mfa\OtpException;
 
 class CheckIP
 {
     protected int $userId;
     protected string $ip;
 
-    public static function onBeforeUserLogin(&$arFields)
+    public static function onBeforeUserLogin(&$arFields): void
     {
         if (!Loader::includeModule('security')) {
             return;
         }
 
         $userId = self::getUserIdByLogin($arFields['LOGIN']);
+
+
         if ($userId <= 0) {
             return;
         }
 
         $ip = self::getClientIp();
         $otp = Otp::getByUser($userId);
+
         if (!$otp) {
             return;
         }
 
         if (self::isInternalIp($ip) && $otp->canSkipMandatory()) {
-            $_SESSION['SKIP_OTP_CHECK'] = true;
-            $otp->deactivate(0);
+            try {
+                $_SESSION['SKIP_OTP_CHECK'] = true;
+
+                if ($otp->isActivated()) $otp->deactivate(0);
+            } catch (OtpException $e) {
+                unset($_SESSION['SKIP_OTP_CHECK']);
+                return;
+            }
         } else {
             unset($_SESSION['SKIP_OTP_CHECK']);
         }
     }
 
-    public static function onAfterUserAuthorize($arParams)
+    public static function onAfterUserAuthorize($arParams): void
     {
         if (!empty($_SESSION['SKIP_OTP_CHECK'])) {
-            $userId = $arParams['user_fields']['ID'];
+            try {
+                $userId = $arParams['user_fields']['ID'];
 
-            $otp = Otp::getByUser($userId);
-            $otp->activate();
+                $otp = Otp::getByUser($userId);
 
-            LocalRedirect('/');
+                if($otp) {
+                    $otp->activate();
+                    unset($_SESSION['SKIP_OTP_CHECK']);
+                }
+
+                LocalRedirect('/');
+            } catch (OtpException $e) {
+                unset($_SESSION['SKIP_OTP_CHECK']);
+                return;
+            }
         }
     }
 
